2cdb45316bbe548f762428b61499ab8f1d79155f
eduardomoisesqa <eduardo.silva.cps@elo.com.br>
2021-10-26 19:39:35 -0300

    aula 4

--- src/main/java/com/rasmoo/cliente/escola/gradecurricular/GradeCurricularApplication.java
+++ src/main/java/com/rasmoo/cliente/escola/gradecurricular/GradeCurricularApplication.java
@@ -2,10 +2,15 @@ package com.rasmoo.cliente.escola.gradecurricular;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class GradeCurricularApplication {
 
+
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(GradeCurricularApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GradeCurricularApplication.class, args);
 	}
